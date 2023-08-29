6b460c79241d7a50802f64bcc0c9a1dcdeadd49f
Saif Ali <81072571+Saif843@users.noreply.github.com>
2023-08-29 07:27:58 +0000

    6 Changed

---
+++ JS_BASICS/6_JS.js
@@ -0,0 +1,11 @@
+console.log(2 < 1);
+
+console.log(null <= 0);
+
+// Comparison and equality check both are differen things
+//Strict check datatype bhi match krti hai (===)
+//== match to krti hai but automatically conversion krdeti hai
+
+console.log("2" === 2);
+
+console.log("2" == 2);
