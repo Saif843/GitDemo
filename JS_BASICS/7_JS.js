84c407d3fe6117fd81f67bb76818ae25b15c5c67
Saif Ali <81072571+Saif843@users.noreply.github.com>
2023-08-29 08:01:50 +0000

    7 Chnaged

---
+++ JS_BASICS/7_JS.js
@@ -0,0 +1,34 @@
+// Kis trhan se data ko memory mai rkha jata hai r access kia jata hai
+//2 categorizations hain iski primitive and non primitive
+
+
+//BigInt, Symbol-making unique wrapper, 
+//If you have to master javascript then master objects, browser events
+
+//null is an object (mean empty not 0)
+//undefined is a type
+
+let id = Symbol('123');
+let accountId = Symbol('123');
+
+console.log(id);
+
+console.log(accountId);
+
+console.log(id == accountId);
+
+const bigNumber = 324354656747;
+console.log(bigNumber);
+
+
+const myArr = ["AHmad","Ali","Khan"];
+
+// const myObj = {
+//     name:"Saif"
+//     age:12,
+// }
+
+
+const myFun = function(){}
+
+//function return data type will be called object function
