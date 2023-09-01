22f987093f8b72e4d84acdbef2db03d190bfb6aa
Saif Ali <81072571+Saif843@users.noreply.github.com>
2023-09-01 11:16:44 +0000

    4

---
+++ JS_Basics_3/4_JS.js
@@ -0,0 +1,20 @@
+// JS ka code kaise execute hota hai
+
+// Call Stack ka execution kaise hote hai
+
+// JS execution context 
+
+
+// Global execution context = this 
+
+// Javascript is single thread
+
+// Function execution context
+// eval execution context
+
+// Memory Creation Phase
+// Execution Phase
+
+// Thread mai execution hota hai javascript ka
+
+
