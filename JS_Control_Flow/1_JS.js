6cff60cc9968e536fc1c64314463a41449c904da
Saif Ali <81072571+Saif843@users.noreply.github.com>
2023-09-02 07:19:53 +0000

    Done

---
+++ JS_Control_Flow/1_JS.js
@@ -0,0 +1,38 @@
+// Key = property
+
+// switch(key){}
+// switch(key){}
+// switch(key){}
+// switch(key){}
+// switch(key){
+//     case value:
+
+// }
+
+
+// truthy and falsy value
+
+// Check array length
+
+let userEmail = [];
+
+if(userEmail.length === 0){
+    console.log("Array is empty");
+}
+
+// Check Object length
+
+
+let userObj = {}
+
+// Object.keys(userObj) this will return/treates an array now you can use length property 
+
+if(Object.keys(userObj).length === 0){
+    console.log("Object is empty");
+}
+
+
+//  Nullish coalescing operator : made for null, undefined 
+// Normally has the when we access the values from the database to stop some problem to our code organization
+
+// null and undefined ki jga flag values dal dein
