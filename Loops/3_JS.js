3949eeddf64a834a5580e6371d64cb04a9e19f64
Saif Ali <81072571+Saif843@users.noreply.github.com>
2023-09-02 19:14:14 +0000

    D

---
+++ Loops/3_JS.js
@@ -0,0 +1,55 @@
+// ArraY SPECIFIC LOOPS
+// Array and objects are the most powerful tools in javascript
+
+// ["",{},""]
+// [{},{},{},{},{}]
+
+// let arr = ["Saif",3,'ALi']
+// for(let v of arr){
+//     console.log(v)
+// }
+
+// num of numbers
+// greet of greetings = Programmers mostly use this name for this type of loop
+
+// Maps are like arrays
+
+// Maps: order and unique value 
+
+
+// [key,value] = array destructuring
+// for(const [key,value] of myMap){
+//     // console.log(key ,`:-` , value)
+// }
+
+
+
+// We cannot use for of for the objects
+
+// We here use for in loops
+
+
+
+// for(const key in myObj){
+    // console.log(key)
+    // console.log(`${key} shortcut is for ${myObj[key]}`)
+//     console.log(myObj[key])
+// }
+
+
+
+// Array ki bhi keys hoti hain
+// Object ko is liye design kia gya tha k  array ki keys 0 se start hoti hain r by default number hi hoti hain
+
+// Isi liye object mai ap jo mrzi keys dal skte hain
+
+// for of and for in loop mai major difference y hai k aik mai apke pas values ati hain aik mai apke pas keys ati hain
+
+// for(const key in programming){
+//     console.log(programming[key]);
+// }
+
+
+
+
+
