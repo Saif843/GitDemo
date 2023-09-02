4ea3f03609d0408996fe457da89687c98f768454
Saif Ali <81072571+Saif843@users.noreply.github.com>
2023-09-02 07:58:33 +0000

    Loop

--- Loops/1_JS.js
+++ Loops/1_JS.js
@@ -1,1 +1,26 @@
 // loops = iteration
+
+
+
+// for(let i = 0 ; i < 10; i++){
+//     const element = i
+//     if(i == 5){
+//         console.log("Saif is a great guy");
+//     }
+//     console.log(element);
+// }
+
+
+for(let i = 0 ; i < 10 ; i++){
+    console.log("----------------------------------------")
+    console.log(`The inner : ${i}`);
+    console.log("----------------------------------------")
+    for(let j = 0 ; j < 10 ; j++){
+        console.log(`The outer j : ${j}, The inner : ${i}`);
+        console.log(`${i} * ${j} = ${i * j}`);
+    }
+}
+
+
+// undefined is a problematic thing itself
+// continue is just like yr aik bar maaf krdo
