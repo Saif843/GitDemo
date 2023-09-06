b72757456da8526ffe951102e6a2e2b4dd9f07d1
Saif Ali <81072571+Saif843@users.noreply.github.com>
2023-09-06 17:36:24 +0000

    D

---
+++ Projects/set4.js
@@ -0,0 +1,6 @@
+// core kafi ziada hai but complex nh
+
+// const randomNumber = console.log(parseInt(Math.random() *  10 OR 100 + 1)) //* is liye kiya hai point ko right side pr lekr jane k liye 10 se 1 place 100 se 2 place and so on. + 1 is liye kiya hai k 100 to aye 0 na aye
+
+// const randomNumber = (parseInt(Math.random() *  10 OR 100 + 1))
+
