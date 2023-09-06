bb5dffa4a5956ef3fa2882f3afd3ff68fc71c8eb
Saif Ali <81072571+Saif843@users.noreply.github.com>
2023-09-06 14:52:33 +0000

    D

---
+++ Projects/set3.js
@@ -0,0 +1,17 @@
+// Timing wla kam kam JS mai bht complex and interesting hai
+
+// const clock = document.getElementById('clocy') OR const clock = document.querySelector('#clocy')
+
+// let date = new Date()
+// console.log(date.toLocaleString());
+
+// I want this date need hr bar run ho hr bar date ati rahe
+// yha pr interviewer apse sui ka puch rha hai r ap talwar nikal rhe hain
+// ap seeda server pr chle gye cron job keh kr aise nh krna apne
+
+// setInterval(function(){
+//     let date = new Date()
+// console.log(date.toLocaleTimeString());
+
+//     clock.innerHTML = date.toLocaleTimeString()
+// },1000)
