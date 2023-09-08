6388dc453366a8e9833eedc6867c222d2645aad2
Saif Ali <81072571+Saif843@users.noreply.github.com>
2023-09-08 09:47:58 +0000

    DO

--- Projects/set4.js
+++ Projects/set4.js
@@ -1,6 +1,12 @@
+// function(e) idr e ka mtlb idr event click hoga
+
 // core kafi ziada hai but complex nh
 
 // const randomNumber = console.log(parseInt(Math.random() *  10 OR 100 + 1)) //* is liye kiya hai point ko right side pr lekr jane k liye 10 se 1 place 100 se 2 place and so on. + 1 is liye kiya hai k 100 to aye 0 na aye
 
 // const randomNumber = (parseInt(Math.random() *  10 OR 100 + 1))
+
+// const submit = document.querySelector('#subt')
+
+// const guessSlot = document.querySelector('.gues')
 
